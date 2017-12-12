4916a9f36350c50ac37af0eeac2306984fd29f45
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-12-12 21:27:30 +0200

    Update styles

--- inc/header.php
+++ inc/header.php
@@ -32,7 +32,7 @@
   $title = ($isHomepage) ? $siteName : $pageName.' : '.$siteName;
 ?>
 <!doctype html>
-<html class="-device_<?php echo $device; ?> no-js" lang="">
+<html class="l-html -device_<?php echo $device; ?> no-js" lang="ru-RU">
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -64,7 +64,7 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-2.1.4.min.js"><\/script>')</script>
 </head>
-<body class="-page_<?php echo $uri; ?><?php echo (!$isHomepage) ? ' -page_inner' : ''; ?>">
+<body class="l-body -page_<?php echo $uri; ?><?php echo (!$isHomepage) ? ' -page_inner' : ''; ?>">
   <!--[if lt IE 8]>
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
